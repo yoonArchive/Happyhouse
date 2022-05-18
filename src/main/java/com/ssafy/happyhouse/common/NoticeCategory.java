f85efc95566d63263e99eac43d1279c1f23ead6d
jeonyuneo <piq2255@gmail.com>
2022-05-18 19:22:50 +0900

    fix: NoticeCategory 수정

--- src/main/java/com/ssafy/happyhouse/common/NoticeCategory.java
+++ src/main/java/com/ssafy/happyhouse/common/NoticeCategory.java
@@ -1,17 +1,14 @@
 package com.ssafy.happyhouse.common;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum NoticeCategory {
 
     UPDATE("업데이트"),
     BUG("버그 개선"),
     NOTICE("알림");
 
-    @JsonValue
-    private String message;
+    private String category;
 
-    NoticeCategory(String message) {
-        this.message = message;
+    NoticeCategory(String category) {
+        this.category = category;
     }
 }
