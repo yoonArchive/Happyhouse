e93ab3b314d7fcb3f761dd934718b85d37083557
jeonyuneo <piq2255@gmail.com>
2022-05-23 22:05:11 +0900

    feat: 거래 상세조회 날짜 최신순 정렬

--- src/main/java/com/ssafy/happyhouse/model/service/TradeServiceImpl.java
+++ src/main/java/com/ssafy/happyhouse/model/service/TradeServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
