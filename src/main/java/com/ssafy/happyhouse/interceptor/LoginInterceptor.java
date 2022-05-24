0c922ad2fdae49bf4406115b0458ad27c001c10c
jeonyuneo <piq2255@gmail.com>
2022-05-24 15:03:27 +0900

    feat: LoginInterceptor 추가

---
+++ src/main/java/com/ssafy/happyhouse/interceptor/LoginInterceptor.java
@@ -0,0 +1,42 @@
+package com.ssafy.happyhouse.interceptor;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+@Component
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+    private static final String SALT = "happyHouseUser";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object Handler) throws Exception {
+
+        log.debug("preHandle() 메소드 실행");
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        while(headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            System.out.println(name);
+        }
+
+        final String token = request.getHeader("access-token");
+        log.debug("토큰 정보 : {}",token);
+        //토큰이 유효한 경우 정상적으로 요청 넘김
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(SALT.getBytes("UTF-8")).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+    }
+}
