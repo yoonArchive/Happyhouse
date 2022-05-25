8e9270646494fd6ddeb339a27f1803db63704b0d
jeonyuneo <piq2255@gmail.com>
2022-05-25 13:42:21 +0900

    fix: 충돌 해결

--- src/api/house.js
+++ src/api/house.js
@@ -1,39 +1,39 @@
-import {apiInstance} from "./index.js";
+import { apiInstance } from "./index.js";
 
 const api = apiInstance();
 
 function sidoList(success, fail) {
-    api.get(`/trade/si`).then(success).catch(fail);
+  api.get(`/trade/si`).then(success).catch(fail);
 }
 
 function gugunList(params, success, fail) {
-    api.get(`/trade/gu`, {params: params}).then(success).catch(fail);
+  api.get(`/trade/gu`, { params: params }).then(success).catch(fail);
 }
 
 function dongList(params, success, fail) {
-    api.get(`/trade/dong`, {params: params}).then(success).catch(fail);
+  api.get(`/trade/dong`, { params: params }).then(success).catch(fail);
 }
 
-function search(params, success, fail) {
-    api.get(`/trade/search`, {params: params}).then(success).catch(fail);
+function showDetail(params, success, fail) {
+  api.get(`/trade/${params}`).then(success).catch(fail);
 }
 
 async function houseListByDong(params, success, fail) {
-    await api
-        .get(`/trade/search/dong`, {params: params})
-        .then(success)
-        .catch(fail);
+  await api
+    .get(`/trade/search/dong`, { params: params })
+    .then(success)
+    .catch(fail);
 }
 
 async function ListByKeyword(params, success, fail) {
-    await api.get(`trade/search`, {params: params}).then(success).catch(fail);
+  await api.get(`trade/search`, { params: params }).then(success).catch(fail);
 }
 
 export {
-    sidoList,
-    gugunList,
-    dongList,
-    search,
-    houseListByDong,
-    ListByKeyword,
+  sidoList,
+  gugunList,
+  dongList,
+  showDetail,
+  houseListByDong,
+  ListByKeyword,
 };
